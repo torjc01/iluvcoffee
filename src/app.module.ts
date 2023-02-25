6156e65ed8529bd19f9400d5cc62990e7ad96782
Julio Cesar Torres <julio.cesartorres@mcn.gouv.qc.ca>
2023-02-25 01:16:48 -0500

    [func] inlcui suporte a banco de dados
    Signed-off-by: Julio Cesar Torres <julio.cesartorres@mcn.gouv.qc.ca>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 
+require('dotenv').config();
+
 @Module({
-  imports: [CoffeesModule],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres', 
+    host: process.env.POSTGRES_HOST, 
+    port: parseInt(process.env.POSTGRES_PORT), 
+    username: process.env.POSTGRES_USERNAME, 
+    password: process.env.POSTGRES_PASSWORD, 
+    database: process.env.POSTGRES_DATABASE, 
+    autoLoadEntities: true, 
+    synchronize: Boolean(process.env.SYNCHRONIZE),  // transformar em variavel de ambiente, e desabilitar em produção
+  }),
+    CoffeesModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
