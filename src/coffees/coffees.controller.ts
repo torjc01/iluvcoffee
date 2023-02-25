444d422572a654af88af23870e444ff387167cc6
Julio Cesar Torres <julio.cesartorres@mcn.gouv.qc.ca>
2023-02-25 01:18:26 -0500

    [func] adiciona paginação
    Signed-off-by: Julio Cesar Torres <julio.cesartorres@mcn.gouv.qc.ca>

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Query, Res } from '@nestjs/common';
+import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto/pagination-query.dto';
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
@@ -22,9 +23,9 @@ export class CoffeesController {
     } 
     */
    @Get()
-   findAll(@Query() paginationQuery){
+   findAll(@Query() paginationQuery: PaginationQueryDto){
     //const { limit, offset } = paginationQuery; 
-    return this.coffeesService.findAll();
+    return this.coffeesService.findAll(paginationQuery);
     //return `This method returns all coffees, limit ${limit}, offset: ${offset}`; 
    }
 
