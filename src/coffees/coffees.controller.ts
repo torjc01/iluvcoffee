6ac007043498ed9afee2b886a78e884edb8624b6
Julio Cesar Torres <julio.cesartorres@mcn.gouv.qc.ca>
2023-07-05 00:49:55 -0400

    [func] inclui service
    Signed-off-by: Julio Cesar Torres <julio.cesartorres@mcn.gouv.qc.ca>

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,8 +1,14 @@
 import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Query, Res } from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
 
 @Controller('coffees')
 export class CoffeesController {
 
+    /**
+     * Construtor, que está servindo para injetar a funcionalidade do CoffeeService no controller.
+     * @param coffeeService 
+     */
+    constructor(private readonly coffeeService: CoffeesService){}
     /**
      * O decorator @Res permite usar a sintaxe nativa da biblioteca http em uso, no caso default, Express.js. 
      * Abaixo a resposta é fornecida pela sintaxe nativa, ao invés da sintaxe NestJS. 
@@ -11,30 +17,35 @@ export class CoffeesController {
      */
     @Get()
     findAll(@Query() paginationQuery){
-        const { limit, offset } = paginationQuery;
+        // const { limit, offset } = paginationQuery;
         // response.status(200).send("This action returns all coffees");
-        return `This action returns all coffees: limit ${limit}, offset ${offset}`;
+        // return `This action returns all coffees: limit ${limit}, offset ${offset}`;
+        return this.coffeeService.findAll(); 
     }
 
     @Get(':id')
     findOne(@Param('id') id: string){
-        return `This action returns #${id} coffee`; 
+        // return `This action returns #${id} coffee`;
+        return this.coffeeService.findOne(id); 
     }
 
     @Post()
-    @HttpCode(HttpStatus.GONE)
+    //@HttpCode(HttpStatus.GONE)
     create(@Body() body){
-        return body;
-        // return `This action creates a coffee`; 
+        // return `This action creates a coffee`;
+        // return body;
+        return this.coffeeService.create(body); 
     }
 
     @Patch(':id')
     update(@Param('id') id: string, @Body() body){
-        return `This action updates #${id} coffee`;
+        // return `This action updates #${id} coffee`;
+        return this.coffeeService.update(id, body); 
     }
 
     @Delete(':id')
     remove(@Param('id') id: string){
-        return `This action deletes #${id} coffee`;
+        // return `This action deletes #${id} coffee`;
+        return this.coffeeService.remove(id);
     }
 }
