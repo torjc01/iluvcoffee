a0c7bae2bad9d9655f6e82885d79761b78bc4ae7
Julio Cesar Torres <julio.cesartorres@mcn.gouv.qc.ca>
2023-07-05 14:23:45 -0400

    [func] suprime pwd
    Signed-off-by: Julio Cesar Torres <julio.cesartorres@mcn.gouv.qc.ca>

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,7 @@ import { CoffeesModule } from './coffees/coffees.module';
       host: 'localhost', 
       port: 5432, 
       username: 'postgres', 
-      password: '6mRsc46ozSNfPnjtSRrMKsbf7qEjEKmKphSk5CzQuN9FkORlMbzWnoB42UyeRGmA', 
+      password: '', 
       database: 'postgres', 
       autoLoadEntities: true, 
       synchronize: true  // Disable when in production environment
